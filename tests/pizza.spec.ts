519c01d74831a6ad3537989977b042ef557fc82f
elsbels64 <wirth10@byu.edu>
2026-02-10 20:39:50 -0700

    added simple tests for about and history

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -7,6 +7,12 @@ test('home page', async ({ page }) => {
   expect(await page.title()).toBe('JWT Pizza');
 });
 
+test('menu page', async ({ page }) => {
+  await page.goto('/menu');
+
+  await expect(page.locator('form')).toContainText('Pick your store and pizzas from below. Remember to order extra for a midnight party.');
+});
+
 async function basicInit(page: Page) {
   let loggedInUser: User | undefined;
   const validUsers: Record<string, User> = { 'd@jwt.com': { id: '3', name: 'Kai Chen', email: 'd@jwt.com', password: 'a', roles: [{ role: Role.Diner }] } };
@@ -106,38 +112,6 @@ test('login', async ({ page }) => {
   await expect(page.getByRole('link', { name: 'KC' })).toBeVisible();
 });
 
-test('purchase with login', async ({ page }) => {
-  await basicInit(page);
-
-  // Go to order page
-  await page.getByRole('button', { name: 'Order now' }).click();
-
-  // Create order
-  await expect(page.locator('h2')).toContainText('Awesome is a click away');
-  await page.getByRole('combobox').selectOption('4');
-  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
-  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
-  await expect(page.locator('form')).toContainText('Selected pizzas: 2');
-  await page.getByRole('button', { name: 'Checkout' }).click();
-
-  // Login
-  await page.getByPlaceholder('Email address').click();
-  await page.getByPlaceholder('Email address').fill('d@jwt.com');
-  await page.getByPlaceholder('Email address').press('Tab');
-  await page.getByPlaceholder('Password').fill('a');
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  // Pay
-  await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
-  await expect(page.locator('tbody')).toContainText('Veggie');
-  await expect(page.locator('tbody')).toContainText('Pepperoni');
-  await expect(page.locator('tfoot')).toContainText('0.008 ₿');
-  await page.getByRole('button', { name: 'Pay now' }).click();
-
-  // Check balance
-  await expect(page.getByText('0.008')).toBeVisible();
-});
-
 test('register', async ({ page }) => {
   let loggedInUser: User | undefined;
   const validUsers: Record<string, User> = { 'new@jwt.com': { id: '4', name: 'New User', email: 'new@jwt.com', password: 'a', roles: [{ role: Role.Diner }] } };
@@ -182,4 +156,46 @@ test('logout', async ({ page }) => {
     - link "Register":
       - /url: /register
     `);
+});
+
+test('purchase with login', async ({ page }) => {
+  await basicInit(page);
+
+  // Go to order page
+  await page.getByRole('button', { name: 'Order now' }).click();
+
+  // Create order
+  await expect(page.locator('h2')).toContainText('Awesome is a click away');
+  await page.getByRole('combobox').selectOption('4');
+  await page.getByRole('link', { name: 'Image Description Veggie A' }).click();
+  await page.getByRole('link', { name: 'Image Description Pepperoni' }).click();
+  await expect(page.locator('form')).toContainText('Selected pizzas: 2');
+  await page.getByRole('button', { name: 'Checkout' }).click();
+
+  // Login
+  await page.getByPlaceholder('Email address').click();
+  await page.getByPlaceholder('Email address').fill('d@jwt.com');
+  await page.getByPlaceholder('Email address').press('Tab');
+  await page.getByPlaceholder('Password').fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Pay
+  await expect(page.getByRole('main')).toContainText('Send me those 2 pizzas right now!');
+  await expect(page.locator('tbody')).toContainText('Veggie');
+  await expect(page.locator('tbody')).toContainText('Pepperoni');
+  await expect(page.locator('tfoot')).toContainText('0.008 ₿');
+  await page.getByRole('button', { name: 'Pay now' }).click();
+
+  // Check balance
+  await expect(page.getByText('0.008')).toBeVisible();
+});
+
+test('about page', async ({ page }) => {
+  await page.goto('/about');
+  await expect(page.getByRole('main')).toContainText('Our employees');;
+});
+
+test('history page', async ({ page }) => {
+  await page.goto('/history');
+  await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
 });
