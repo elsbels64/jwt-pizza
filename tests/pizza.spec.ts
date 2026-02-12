729964a910d4d491404ec0cb351924ea75c34e2f
elsbels64 <wirth10@byu.edu>
2026-02-11 18:11:49 -0700

    added admin test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -198,4 +198,50 @@ test('about page', async ({ page }) => {
 test('history page', async ({ page }) => {
   await page.goto('/history');
   await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+} );
+
+test('login admin and open admin page', async ({ page }) => {
+  await page.goto('/');
+  //login admin
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  //go to admin page
+  //   /api/franchise[get]
+
+  await page.getByRole('link', { name: 'Admin' }).click();
+  await expect(page.locator('h3')).toContainText('Franchises');
+  
+  //add a franchise
+  // await page.getByRole('button', { name: 'Add Franchise' }).click();
+  // await page.getByRole('textbox', { name: 'franchise name' }).click();
+  // await page.getByRole('textbox', { name: 'franchise name' }).fill('New Franchise');
+  // await page.getByRole('textbox', { name: 'franchisee admin email' }).click();
+  // await page.getByRole('textbox', { name: 'franchisee admin email' }).fill('d@jwt.com');
+  //  /api/franchise[post]
+  // await page.getByRole('button', { name: 'Create' }).click();
+  // await expect(page.getByRole('table')).toContainText('New Franchise');
+
+  //filter 
+  //   /api/franchise?name={name}[get]???
+  // await page.getByRole('textbox', { name: 'Filter franchises' }).click();
+  // await page.getByRole('textbox', { name: 'Filter franchises' }).fill('pizzaPocket');
+  // await page.getByRole('button', { name: 'Submit' }).click();
+  // await expect(page.locator('tbody')).toContainText('pizzaPocket');
+  // await page.getByRole('textbox', { name: 'Filter franchises' }).dblclick();
+  // await page.getByRole('textbox', { name: 'Filter franchises' }).fill('');
+  // await page.getByRole('button', { name: 'Submit' }).click();
+
+  //delete a franchise
+//   /api/franchise/{id}[delete]
+
+  // await expect(page.getByRole('table')).toContainText('New Franchise');
+  // await page.getByRole('row', { name: 'New Franchise pizza diner' }).getByRole('button').click();
+  // await expect(page.getByRole('heading')).toContainText('Sorry to see you go');
+  // await page.getByRole('button', { name: 'Close' }).click();
+  // await expect(page.locator('tbody')).toContainText('pizzaPocket');
 });
