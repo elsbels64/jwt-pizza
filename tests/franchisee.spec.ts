048272a8b0bcecc342e586e0168efe72d700ebe8
elsbels64 <wirth10@byu.edu>
2026-02-11 22:34:22 -0700

    create store mocked out

--- tests/franchisee.spec.ts
+++ tests/franchisee.spec.ts
@@ -4,18 +4,18 @@ import { Role, User } from '../src/service/pizzaService';
 async function basicInit(page: Page) {
     let loggedInUser: User | undefined;
     const validUsers: Record<string, User> = { "f@jwt.com": { id: '4', name: "pizza franchisee", email: "f@jwt.com", password: "franchisee", roles: ([{ role: Role.Franchisee }]) } };
-  
+
     //PUT /api/auth HTTP/1.1
     // {"user":{"id":4,"name":"pizza franchisee","email":"f@jwt.com","roles":[{"role":"diner"},{"objectId":1,"role":"franchisee"}]},"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwibmFtZSI6InBpenphIGZyYW5jaGlzZWUiLCJlbWFpbCI6ImZAand0LmNvbSIsInJvbGVzIjpbeyJyb2xlIjoiZGluZXIifSx7Im9iamVjdElkIjoxLCJyb2xlIjoiZnJhbmNoaXNlZSJ9XSwiaWF0IjoxNzcwODcwMjc0fQ.gXCU7dkTuzWJ6nhPPFz8Fd0lGRsfp50moNiVfoFyruI"}
 
     let franchises = [
-    {
-      id: '1',
-      name: 'pizzaPocket',
-      admins: [{ id: '4', name: "pizza franchisee", email: "f@jwt.com" }],
-      stores: [{ id: '1', name: "SLC", totalRevenue: 0 }]
-    }
-  ];
+        {
+            id: '1',
+            name: 'pizzaPocket',
+            admins: [{ id: '4', name: "pizza franchisee", email: "f@jwt.com" }],
+            stores: [{ id: '1', name: "SLC", totalRevenue: 0 }]
+        }
+    ];
 
     await page.route('*/**/api/auth', async (route) => {
         const loginReq = route.request().postDataJSON();
@@ -50,27 +50,26 @@ async function basicInit(page: Page) {
     await page.route('*/**/api/franchise**', async (route) => {
         if (route.request().method() === 'GET') {
             if (route.request().url().includes("4")) {
-            await route.fulfill( {json: franchises} );
+                await route.fulfill({ json: franchises });
             } else {
                 await route.fulfill({ json: { franchises, more: false } });
             }
             return;
+        } else if (route.request().method() === 'POST') {
+            // POST /api/franchise/1/store HTTP/1.1
+            // {"id":3,"franchiseId":1,"name":"new store"}
+            // [{"id":1,"name":"pizzaPocket","admins":[{"id":4,"name":"pizza franchisee","email":"f@jwt.com"}],"stores":[{"id":2,"name":"SLC","totalRevenue":0},{"id":3,"name":"new store","totalRevenue":0}]}]
+
+            franchises[0].stores.push({ id: '3', name: "new store", totalRevenue: 0 });
+            await route.fulfill({ json: { id: '3', franchiseId: '1', name: "new store" } });
+            return;
+        } else if (route.request().method() === 'DELETE') {
+            //DELETE /api/franchise/1/store/3 HTTP/1.1
+            // {"message":"store deleted"}
+            franchises[0].stores = franchises[0].stores.filter(s => s.id !== '3');
+            await route.fulfill({ json: { message: "store deleted" } });
+            return;
         }
-
-
-
-        // POST /api/franchise/1/store HTTP/1.1
-        // {"id":3,"franchiseId":1,"name":"new store"}
-
-
-
-        // [{"id":1,"name":"pizzaPocket","admins":[{"id":4,"name":"pizza franchisee","email":"f@jwt.com"}],"stores":[{"id":2,"name":"SLC","totalRevenue":0},{"id":3,"name":"new store","totalRevenue":0}]}]
-
-
-        //DELETE /api/franchise/1/store/3 HTTP/1.1
-        // {"message":"store deleted"}
-
-
     });
 
     await page.goto('/');
@@ -94,19 +93,19 @@ test("Franchisee can login and view their franchises and stores", async ({ page 
 
 
     // create new store
-    // await page.getByRole('button', { name: 'Create store' }).click();
-    // await page.getByRole('textbox', { name: 'store name' }).click();
-    // await page.getByRole('textbox', { name: 'store name' }).fill('new store');
-    // await page.getByRole('button', { name: 'Create' }).click();
-    // await expect(page.locator('tbody')).toContainText('new store');
+    await page.getByRole('button', { name: 'Create store' }).click();
+    await page.getByRole('textbox', { name: 'store name' }).click();
+    await page.getByRole('textbox', { name: 'store name' }).fill('new store');
+    await page.getByRole('button', { name: 'Create' }).click();
+    await expect(page.locator('tbody')).toContainText('new store');
 
     // // close store
-    // await page.getByRole('row', { name: 'new store 0 ₿ Close' }).getByRole('button').click();
-    // await expect(page.getByRole('heading')).toContainText('Sorry to see you go');
-    // await expect(page.getByRole('main')).toContainText('pizzaPocket');
-    // await expect(page.getByRole('main')).toContainText('new store');
-    // await page.getByRole('button', { name: 'Close' }).click();
-    // await expect(page.locator('tbody')).toContainText('SLC');
+    await page.getByRole('row', { name: 'new store 0 ₿ Close' }).getByRole('button').click();
+    await expect(page.getByRole('heading')).toContainText('Sorry to see you go');
+    await expect(page.getByRole('main')).toContainText('pizzaPocket');
+    await expect(page.getByRole('main')).toContainText('new store');
+    await page.getByRole('button', { name: 'Close' }).click();
+    await expect(page.locator('tbody')).toContainText('SLC');
 
     // // logout
     // await page.getByRole('link', { name: 'Logout' }).click();    
