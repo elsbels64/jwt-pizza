dd0296a0bc8e8444aa613217f1d235ed7860ccb7
elsbels64 <wirth10@byu.edu>
2026-04-02 00:27:09 -0600

    update timeout time

--- playwright.integration.config.js
+++ playwright.integration.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  timeout:5000,
+  timeout:30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -76,6 +76,6 @@ export default defineConfig({
     command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-    timeout: process.env.PWDEBUG ? 120000 : 10000,
+    timeout: process.env.PWDEBUG ? 120000 : 30000,
   },
 });
