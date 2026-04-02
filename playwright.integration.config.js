85bf37dc8a361d3048b92bc4afa11e069a6852f3
elsbels64 <wirth10@byu.edu>
2026-04-02 01:33:53 -0600

    more logs

--- playwright.integration.config.js
+++ playwright.integration.config.js
@@ -32,6 +32,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'http://localhost:5173',
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',  // ← add this
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
