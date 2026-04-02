397e4b9fa0a15e3e60e10efdf8afcf144292a498
elsbels64 <wirth10@byu.edu>
2026-04-02 16:25:27 -0600

    change service url for tests

--- playwright.integration.config.js
+++ playwright.integration.config.js
@@ -77,10 +77,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'VITE_PIZZA_SERVICE_URL=http://127.0.0.1:3000 npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-    timeout: process.env.PWDEBUG ? 120000 : 30000,
+  // playwright.integration.config.js
+    webServer: {
+        command: 'VITE_PIZZA_SERVICE_URL=http://localhost:3000 npm run dev',
+        url: 'http://localhost:5173',
+        reuseExistingServer: !process.env.CI,
+        timeout: process.env.PWDEBUG ? 120000 : 30000,
     },
 });
